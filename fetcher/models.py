287bb56c460a54b50ebc2c7636a611b20e530492
kp <kp@x>
2009-03-23 23:31:39 +0100

    Doprecyzowanie modelu.

--- fetcher/models.py
+++ fetcher/models.py
@@ -5,7 +5,7 @@ class Lecture:
         self.name = None 
         self.link = None
         self.words = None
-	# map<string,int>
+	# map<string,int>. wyraz (malymi literami) -> ile razy wystapil w wykladzie
  
 class Course: 
     def __init__(self): 
